InsightsDev-dev/presto-1

Support index joins probing with subsets of join keys
This allows us to push index join keys through query shapes that may not allow
all keys to pass through. For example,

  SELECT *
  FROM table1
  JOIN (
    SELECT c1, COUNT(*) AS c2
    FROM table2
    GROUP BY c1
  ) t2
  ON table1.c1 = t2.c1 AND table.c2 = t2.c2;

will be able to send a index join probe key through the aggregation with column 'c1'.
Other similarly supported shapes include joins and projections.

presto-main/src/main/java/com/facebook/presto/operator/index/DynamicTupleFilterFactory.java
@@ -1,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator.index;
+
+import com.facebook.presto.operator.OperatorFactory;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+import static com.facebook.presto.operator.FilterAndProjectOperator.FilterAndProjectOperatorFactory;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class DynamicTupleFilterFactory
+{
+    private final int filterOperatorId;
+    private final int[] tupleFilterChannels;
+    private final int[] outputFilterChannels;
+    private final List<Type> outputTypes;
+
+    public DynamicTupleFilterFactory(int filterOperatorId, int[] tupleFilterChannels, int[] outputFilterChannels, List<Type> outputTypes)
+    {
+        checkNotNull(tupleFilterChannels, "tupleFilterChannels is null");
+        checkArgument(tupleFilterChannels.length > 0, "Must have at least one tupleFilterChannel");
+        checkNotNull(outputFilterChannels, "outputFilterChannels is null");
+        checkArgument(outputFilterChannels.length == tupleFilterChannels.length, "outputFilterChannels must have same length as tupleFilterChannels");
+        checkNotNull(outputTypes, "outputTypes is null");
+        checkArgument(outputTypes.size() >= outputFilterChannels.length, "Must have at least as many output channels as those used for filtering");
+
+        this.filterOperatorId = filterOperatorId;
+        this.tupleFilterChannels = tupleFilterChannels.clone();
+        this.outputFilterChannels = outputFilterChannels.clone();
+        this.outputTypes = ImmutableList.copyOf(outputTypes);
+    }
+
+    public OperatorFactory filterWithTuple(Page tuplePage)
+    {
+        Page normalizedTuplePage = normalizeTuplePage(tuplePage);
+        TupleFilterProcessor processor = new TupleFilterProcessor(normalizedTuplePage, outputTypes, outputFilterChannels);
+        return new FilterAndProjectOperatorFactory(filterOperatorId, processor, outputTypes);
+    }
+
+    private Page normalizeTuplePage(Page tuplePage)
+    {
+        Block[] normalizedBlocks = new Block[tupleFilterChannels.length];
+        for (int i = 0; i < tupleFilterChannels.length; i++) {
+            normalizedBlocks[i] = tuplePage.getBlock(tupleFilterChannels[i]);
+        }
+        return new Page(normalizedBlocks);
+    }
+}

presto-main/src/main/java/com/facebook/presto/operator/index/IndexBuildDriverFactoryProvider.java
@@ -15,7 +15,9 @@ package com.facebook.presto.operator.index;
 
 import com.facebook.presto.operator.DriverFactory;
 import com.facebook.presto.operator.OperatorFactory;
+import com.facebook.presto.spi.Page;
 import com.facebook.presto.spi.type.Type;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
@@ -31,15 +33,19 @@ public class IndexBuildDriverFactoryProvider
     private final boolean inputDriver;
     private final List<OperatorFactory> coreOperatorFactories;
     private final List<Type> outputTypes;
+    private final Optional<DynamicTupleFilterFactory> dynamicTupleFilterFactory;
 
-    public IndexBuildDriverFactoryProvider(int outputOperatorId, boolean inputDriver, List<OperatorFactory> coreOperatorFactories)
+    public IndexBuildDriverFactoryProvider(int outputOperatorId, boolean inputDriver, List<OperatorFactory> coreOperatorFactories, Optional<DynamicTupleFilterFactory> dynamicTupleFilterFactory)
     {
-        this.outputOperatorId = outputOperatorId;
-        this.inputDriver = inputDriver;
         checkNotNull(coreOperatorFactories, "coreOperatorFactories is null");
         checkArgument(!coreOperatorFactories.isEmpty(), "coreOperatorFactories is empty");
+        checkNotNull(dynamicTupleFilterFactory, "dynamicTupleFilterFactory is null");
+
+        this.outputOperatorId = outputOperatorId;
+        this.inputDriver = inputDriver;
         this.coreOperatorFactories = ImmutableList.copyOf(coreOperatorFactories);
         this.outputTypes = ImmutableList.copyOf(this.coreOperatorFactories.get(this.coreOperatorFactories.size() - 1).getTypes());
+        this.dynamicTupleFilterFactory = dynamicTupleFilterFactory;
     }
 
     public List<Type> getOutputTypes()
@@ -47,7 +53,7 @@ public class IndexBuildDriverFactoryProvider
         return outputTypes;
     }
 
-    public DriverFactory create(IndexSnapshotBuilder indexSnapshotBuilder)
+    public DriverFactory createSnapshot(IndexSnapshotBuilder indexSnapshotBuilder)
     {
         checkArgument(indexSnapshotBuilder.getOutputTypes().equals(outputTypes));
         return new DriverFactory(inputDriver, false, ImmutableList.<OperatorFactory>builder()
@@ -56,11 +62,18 @@ public class IndexBuildDriverFactoryProvider
                 .build());
     }
 
-    public DriverFactory create(PageBuffer pageBuffer)
+    public DriverFactory createStreaming(PageBuffer pageBuffer, Page indexKeyTuple)
     {
-        return new DriverFactory(inputDriver, false, ImmutableList.<OperatorFactory>builder()
-                .addAll(coreOperatorFactories)
-                .add(new PageBufferOperatorFactory(outputOperatorId, pageBuffer))
-                .build());
+        ImmutableList.Builder<OperatorFactory> operatorFactories = ImmutableList.<OperatorFactory>builder()
+                .addAll(coreOperatorFactories);
+
+        if (dynamicTupleFilterFactory.isPresent()) {
+            // Bind in a dynamic tuple filter if necessary
+            operatorFactories.add(dynamicTupleFilterFactory.get().filterWithTuple(indexKeyTuple));
+        }
+
+        operatorFactories.add(new PageBufferOperatorFactory(outputOperatorId, pageBuffer));
+
+        return new DriverFactory(inputDriver, false, operatorFactories.build());
     }
 }

presto-main/src/main/java/com/facebook/presto/operator/index/TupleFilterProcessor.java
@@ -1,0 +1,86 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator.index;
+
+import com.facebook.presto.operator.PageProcessor;
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.PageBuilder;
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.block.BlockBuilder;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Filters out rows that do not match the values from the specified tuple
+ */
+public class TupleFilterProcessor
+        implements PageProcessor
+{
+    private final Page tuplePage;
+    private final List<Type> outputTypes;
+    private final int[] outputTupleChannels;
+
+    public TupleFilterProcessor(Page tuplePage, List<Type> outputTypes, int[] outputTupleChannels)
+    {
+        checkNotNull(tuplePage, "tuplePage is null");
+        checkArgument(tuplePage.getPositionCount() == 1, "tuplePage should only have one position");
+        checkArgument(tuplePage.getChannelCount() > 0, "tuplePage must have at least one channel");
+        checkNotNull(outputTypes, "outputTypes is null");
+        checkNotNull(outputTupleChannels, "outputTupleChannels is null");
+        checkArgument(tuplePage.getChannelCount() == outputTupleChannels.length, "tuplePage and outputTupleChannels have different number of channels");
+        checkArgument(outputTypes.size() >= outputTupleChannels.length, "Must have at least as many output channels as those used for filtering");
+
+        this.tuplePage = tuplePage;
+        this.outputTypes = ImmutableList.copyOf(outputTypes);
+        this.outputTupleChannels = outputTupleChannels;
+    }
+
+    @Override
+    public int process(ConnectorSession session, Page page, int start, int end, PageBuilder pageBuilder)
+    {
+        // TODO: generate bytecode for this in the future
+        for (int position = start; position < end; position++) {
+            if (matches(position, page)) {
+                pageBuilder.declarePosition();
+                for (int i = 0; i < outputTypes.size(); i++) {
+                    Type type = outputTypes.get(i);
+                    Block block = page.getBlock(i);
+                    BlockBuilder blockBuilder = pageBuilder.getBlockBuilder(i);
+                    type.appendTo(block, position, blockBuilder);
+                }
+            }
+        }
+
+        return end;
+    }
+
+    private boolean matches(int position, Page page)
+    {
+        for (int i = 0; i < outputTupleChannels.length; i++) {
+            Type type = outputTypes.get(outputTupleChannels[i]);
+            Block outputBlock = page.getBlock(outputTupleChannels[i]);
+            Block singleTupleBlock = tuplePage.getBlock(i);
+            if (!type.equalTo(singleTupleBlock, 0, outputBlock, position)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

ssheafer/presto

Conflicts:
	presto-main/src/main/java/com/facebook/presto/operator/index/IndexSnapshotBuilder.java
	presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
	presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/IndexJoinOptimizer.java

presto-main/src/main/java/com/facebook/presto/operator/index/DynamicTupleFilterFactory.java
@@ -1,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator.index;
+
+import com.facebook.presto.operator.OperatorFactory;
+import com.facebook.presto.operator.Page;
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+import static com.facebook.presto.operator.FilterAndProjectOperator2.FilterAndProjectOperatorFactory2;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class DynamicTupleFilterFactory
+{
+    private final int filterOperatorId;
+    private final int[] tupleFilterChannels;
+    private final int[] outputFilterChannels;
+    private final List<Type> outputTypes;
+
+    public DynamicTupleFilterFactory(int filterOperatorId, int[] tupleFilterChannels, int[] outputFilterChannels, List<Type> outputTypes)
+    {
+        checkNotNull(tupleFilterChannels, "tupleFilterChannels is null");
+        checkArgument(tupleFilterChannels.length > 0, "Must have at least one tupleFilterChannel");
+        checkNotNull(outputFilterChannels, "outputFilterChannels is null");
+        checkArgument(outputFilterChannels.length == tupleFilterChannels.length, "outputFilterChannels must have same length as tupleFilterChannels");
+        checkNotNull(outputTypes, "outputTypes is null");
+        checkArgument(outputTypes.size() >= outputFilterChannels.length, "Must have at least as many output channels as those used for filtering");
+
+        this.filterOperatorId = filterOperatorId;
+        this.tupleFilterChannels = tupleFilterChannels.clone();
+        this.outputFilterChannels = outputFilterChannels.clone();
+        this.outputTypes = ImmutableList.copyOf(outputTypes);
+    }
+
+    public OperatorFactory filterWithTuple(Page tuplePage)
+    {
+        Page normalizedTuplePage = normalizeTuplePage(tuplePage);
+        TupleFilterProcessor processor = new TupleFilterProcessor(normalizedTuplePage, outputTypes, outputFilterChannels);
+        return new FilterAndProjectOperatorFactory2(filterOperatorId, processor, outputTypes);
+    }
+
+    private Page normalizeTuplePage(Page tuplePage)
+    {
+        Block[] normalizedBlocks = new Block[tupleFilterChannels.length];
+        for (int i = 0; i < tupleFilterChannels.length; i++) {
+            normalizedBlocks[i] = tuplePage.getBlock(tupleFilterChannels[i]);
+        }
+        return new Page(normalizedBlocks);
+    }
+}

presto-main/src/main/java/com/facebook/presto/operator/index/IndexBuildDriverFactoryProvider.java
@@ -15,7 +15,9 @@ package com.facebook.presto.operator.index;
 
 import com.facebook.presto.operator.DriverFactory;
 import com.facebook.presto.operator.OperatorFactory;
+import com.facebook.presto.operator.Page;
 import com.facebook.presto.spi.type.Type;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
@@ -31,15 +33,19 @@ public class IndexBuildDriverFactoryProvider
     private final boolean inputDriver;
     private final List<OperatorFactory> coreOperatorFactories;
     private final List<Type> outputTypes;
+    private final Optional<DynamicTupleFilterFactory> dynamicTupleFilterFactory;
 
-    public IndexBuildDriverFactoryProvider(int outputOperatorId, boolean inputDriver, List<OperatorFactory> coreOperatorFactories)
+    public IndexBuildDriverFactoryProvider(int outputOperatorId, boolean inputDriver, List<OperatorFactory> coreOperatorFactories, Optional<DynamicTupleFilterFactory> dynamicTupleFilterFactory)
     {
-        this.outputOperatorId = outputOperatorId;
-        this.inputDriver = inputDriver;
         checkNotNull(coreOperatorFactories, "coreOperatorFactories is null");
         checkArgument(!coreOperatorFactories.isEmpty(), "coreOperatorFactories is empty");
+        checkNotNull(dynamicTupleFilterFactory, "dynamicTupleFilterFactory is null");
+
+        this.outputOperatorId = outputOperatorId;
+        this.inputDriver = inputDriver;
         this.coreOperatorFactories = ImmutableList.copyOf(coreOperatorFactories);
         this.outputTypes = ImmutableList.copyOf(this.coreOperatorFactories.get(this.coreOperatorFactories.size() - 1).getTypes());
+        this.dynamicTupleFilterFactory = dynamicTupleFilterFactory;
     }
 
     public List<Type> getOutputTypes()
@@ -47,7 +53,7 @@ public class IndexBuildDriverFactoryProvider
         return outputTypes;
     }
 
-    public DriverFactory create(IndexSnapshotBuilder indexSnapshotBuilder)
+    public DriverFactory createSnapshot(IndexSnapshotBuilder indexSnapshotBuilder)
     {
         checkArgument(indexSnapshotBuilder.getOutputTypes().equals(outputTypes));
         return new DriverFactory(inputDriver, false, ImmutableList.<OperatorFactory>builder()
@@ -56,11 +62,18 @@ public class IndexBuildDriverFactoryProvider
                 .build());
     }
 
-    public DriverFactory create(PageBuffer pageBuffer)
+    public DriverFactory createStreaming(PageBuffer pageBuffer, Page indexKeyTuple)
     {
-        return new DriverFactory(inputDriver, false, ImmutableList.<OperatorFactory>builder()
-                .addAll(coreOperatorFactories)
-                .add(new PageBufferOperatorFactory(outputOperatorId, pageBuffer))
-                .build());
+        ImmutableList.Builder<OperatorFactory> operatorFactories = ImmutableList.<OperatorFactory>builder()
+                .addAll(coreOperatorFactories);
+
+        if (dynamicTupleFilterFactory.isPresent()) {
+            // Bind in a dynamic tuple filter if necessary
+            operatorFactories.add(dynamicTupleFilterFactory.get().filterWithTuple(indexKeyTuple));
+        }
+
+        operatorFactories.add(new PageBufferOperatorFactory(outputOperatorId, pageBuffer));
+
+        return new DriverFactory(inputDriver, false, operatorFactories.build());
     }
 }

presto-main/src/main/java/com/facebook/presto/operator/index/TupleFilterProcessor.java
@@ -1,0 +1,84 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.operator.index;
+
+import com.facebook.presto.operator.Page;
+import com.facebook.presto.operator.PageBuilder;
+import com.facebook.presto.operator.PageProcessor;
+import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.block.Block;
+import com.facebook.presto.spi.block.BlockBuilder;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Filters out rows that do not match the values from the specified tuple
+ */
+public class TupleFilterProcessor
+        implements PageProcessor
+{
+    private final Page tuplePage;
+    private final List<Type> outputTypes;
+    private final int[] outputTupleChannels;
+
+    public TupleFilterProcessor(Page tuplePage, List<Type> outputTypes, int[] outputTupleChannels)
+    {
+        checkNotNull(tuplePage, "tuplePage is null");
+        checkArgument(tuplePage.getPositionCount() == 1, "tuplePage should only have one position");
+        checkArgument(tuplePage.getChannelCount() > 0, "tuplePage must have at least one channel");
+        checkNotNull(outputTypes, "outputTypes is null");
+        checkNotNull(outputTupleChannels, "outputTupleChannels is null");
+        checkArgument(tuplePage.getChannelCount() == outputTupleChannels.length, "tuplePage and outputTupleChannels have different number of channels");
+        checkArgument(outputTypes.size() >= outputTupleChannels.length, "Must have at least as many output channels as those used for filtering");
+
+        this.tuplePage = tuplePage;
+        this.outputTypes = ImmutableList.copyOf(outputTypes);
+        this.outputTupleChannels = outputTupleChannels;
+    }
+
+    @Override
+    public int process(ConnectorSession session, Page page, int start, int end, PageBuilder pageBuilder)
+    {
+        // TODO: generate bytecode for this in the future
+        for (int position = start; position < end; position++) {
+            if (matches(position, page)) {
+                pageBuilder.declarePosition();
+                for (int i = 0; i < outputTypes.size(); i++) {
+                    Block block = page.getBlock(i);
+                    BlockBuilder blockBuilder = pageBuilder.getBlockBuilder(i);
+                    block.appendTo(position, blockBuilder);
+                }
+            }
+        }
+
+        return end;
+    }
+
+    private boolean matches(int position, Page page)
+    {
+        for (int i = 0; i < outputTupleChannels.length; i++) {
+            Block outputBlock = page.getBlock(outputTupleChannels[i]);
+            Block singleTupleBlock = tuplePage.getBlock(i);
+            if (!singleTupleBlock.equalTo(0, outputBlock, position)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}